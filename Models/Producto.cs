7dda6afa7ff2688bbf6d907bfbad1f62b2e5957f
Cristhian Leandro Góngora Torrez <crisgongor7@gmail.com>
2026-04-22 11:10:03 -0400

    feat: añadir modelo Producto

---
+++ Models/Producto.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProducto.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public DateTime FechaDeAlta { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
